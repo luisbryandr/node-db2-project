cb761e69ec4c183f60422caddcf65e2cfb91cc5b
luisbryandr <luisbdominguezr@gmail.com>
2021-12-08 17:58:32 -0500

    built out model functions

--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require("express")
 
 const server = express()
 
-// DO YOUR MAGIC
+const carsRouter = require('./cars/cars-router');
+
+server.use(express.json());
+
+// server.use('/api/cars', carsRouter)
 
 module.exports = server
